40bd89838417b6269b33f8dd3fa24fce67a0415b
Yae-Htet-Aung <yaetactaung@gmail.com>
2024-03-19 15:10:06 +0630

    viber-added

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <p className="subTitleMM text-center pb-7 font-bold">ဆက်သွယ်ရန်</p>
 
       <div className="flex flex-wrap flex-col lg:flex-row items-center px-5 justify-center gap-5 text-white font-semibold">
-        <Link href={"blogs"}>
+        <Link href="https://viber://chat?number=+959776155260">
           <div className="flex gap-4 md:gap-5 items-center w-[95%] md:min-w-[310px] lg:w-[29%] max-w-[330px] p-3 bg-[#573187] rounded-lg">
             <Image
               src={Viber}
