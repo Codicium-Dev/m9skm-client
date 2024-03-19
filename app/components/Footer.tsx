9d390b89b1d4e971fb68e5046fb1c91facfa07d6
Yae-Htet-Aung <yaetactaung@gmail.com>
2024-03-19 16:15:56 +0630

    viber-2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,18 +5,19 @@ import Telegram from "../../public/assets/footer/footer-telegram.png";
 import Phone from "../../public/assets/footer/footer-phone.png";
 import Link from "next/link";
 const Footer = () => {
-  const viberNumber = "09776155260";
+  const phoneNumber = '09776155260'
+  const viberURL = `viber://add?number=${encodeURIComponent(phoneNumber)}`;
   return (
     <div className="flex flex-col h-auto lg:h-[300px] py-8 ">
       <p className="subTitleMM text-center pb-7 font-bold">ဆက်သွယ်ရန်</p>
 
       <div className="flex flex-wrap flex-col lg:flex-row items-center px-5 justify-center gap-5 text-white font-semibold">
-        <Link href={`viber://add?number=${viberNumber}`}>
+        <Link href={`${viberURL}`}>
           <div className="flex gap-4 md:gap-5 items-center w-[95%] md:min-w-[310px] lg:w-[29%] max-w-[330px] p-3 bg-[#573187] rounded-lg">
             <Image
               src={Viber}
-              width={50}
-              height={50}
+              width={150}
+              height={150}
               alt="viber-logo"
               className="w-10 h-10 md:w-12 md:h-12"
             ></Image>
@@ -28,8 +29,8 @@ const Footer = () => {
           <div className="flex gap-4 md:gap-5 items-center w-[95%] md:min-w-[310px] lg:w-[29%] max-w-[330px] p-3 bg-[#0082be] rounded-lg">
             <Image
               src={Telegram}
-              width={50}
-              height={50}
+              width={150}
+              height={150}
               alt="viber-logo"
               className="w-10 h-10 md:w-12 md:h-12"
             ></Image>
@@ -42,8 +43,8 @@ const Footer = () => {
           <div className="flex gap-4 md:gap-5 items-center w-[95%] md:min-w-[310px] lg:w-[29%] max-w-[330px] p-3 bg-[#58b961] rounded-lg">
             <Image
               src={Phone}
-              width={50}
-              height={50}
+              width={150}
+              height={150}
               alt="viber-logo"
               className="w-10 h-10 md:w-12 md:h-12"
             ></Image>
