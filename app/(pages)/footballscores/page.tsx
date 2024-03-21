2da9b2a9f4f038c68ea99b7bf69401ffa2b0e286
Yae-Htet-Aung <yaetactaung@gmail.com>
2024-03-21 17:27:07 +0630

    scores-page-responsive-ok

--- app/(pages)/footballscores/page.tsx
+++ app/(pages)/footballscores/page.tsx
@@ -1,143 +1,154 @@
+"use client";
 import Navbar from "@/app/components/nav/Navbar";
 import Image from "next/image";
 import Team1 from "../../../public/assets/home-page/team-logo1.png";
 import Team2 from "../../../public/assets/home-page/team-logo2.png";
 import Footer from "@/app/components/Footer";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function page() {
-  const Data = [1, 2, 3, 4, 5, 6];
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://m9.goldenyellowtravel.com/api/v1/matches/list`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        // console.log(res);
+        const fetchedData = res?.data?.data;
+        setMatches(fetchedData);
+        // console.log("fetchedData >> ", fetchedData);
+      })
+      .catch((error) => {
+        console.error("Error fetching news:", error);
+      });
+  }, []);
+
+  // Get today's date
+  const today = new Date();
+  const todayDate = today.toISOString().split("T")[0];
+
+  // Filter the array to include only matches with today's date
+  const todaysMatches = matches.filter(
+    (match: any) => match.date === "2022-04-13"
+  );
+
+  // console.log(todaysMatches);
+
   return (
     <>
       <Navbar visibleDefault={true} fixed={false} />
 
-      <div className="flex flex-col px-[190px]">
-        <p className="text-3xl font-bold pb-10">ယနေ့ပွဲစဉ်များ</p>
+      <div
+        className={`flex flex-col px-3 md:px-5 lg:px-20 xl:px-[190px] pb-7 md:pb-0 ${
+          todaysMatches.length === 0 ? "hidden" : "block"
+        }`}
+      >
+        <p className="md:text-xl lg:text-3xl font-bold pb-3 md:pb-5 lg:pb-10">
+          ယနေ့ပွဲစဉ်များ
+        </p>
 
-        <div className="flex flex-col gap-10 pb-[96px]">
+        <div className="flex flex-col gap-5 md:pb-10 lg:pb-[96px]">
           {/* card */}
-          <div className="bg-white flex items-center gap-5 md:gap-5 justify-center lg:py-0 h-fit md:h-[180px]">
-            <div className="flex items-center gap-2 w-full px-16 h-full ">
-              <div className="flex w-full ">
-                <div className="flex justify-center w-[40%]">
-                  <div>
-                    <Image
-                      width={200}
-                      height={200}
-                      src={Team1}
-                      alt="hero_app"
-                      className="object-cover w-[120px] h-[120px] p-1"
-                    />
+          {todaysMatches?.map((item: any) => (
+            <div
+              key={item.id}
+              className="bg-white flex items-center gap-5 md:gap-5 justify-center lg:py-0 h-[100px] md:h-[140px] lg:h-[160px] xl:h-[180px]"
+            >
+              <div className="flex items-center gap-4 w-full lg:px-5 xl:px-16 h-full ">
+                <div className="flex w-full ">
+                  <div className="flex justify-center w-full lg:w-[35%]">
+                    <div className="w-[100px] h-[100px] md:w-[120px] md:h-[120px] p-5 md:p-1">
+                      <Image
+                        width={200}
+                        height={200}
+                        src={item.home_team_photo}
+                        alt="hero_app"
+                        className="object-contain w-full h-full"
+                      />
+                    </div>
+                  </div>
+                  <div className="md:flex hidden items-center justify-center font-semibold text-center text-md xl:text-lg align-middle w-[60%] lg:w-[65%] ">
+                    {item.home_team_name}
                   </div>
                 </div>
-                <div className="flex items-center justify-center font-semibold text-center text-xl align-middle w-[60%]">
-                  Manchester United
+                <div className="flex items-center justify-center w-[250px] xl:w-[400px] h-full font-semibold text-[#fea61f] bg-black">
+                  <p className="w-full text-center">{item.time.substr(0, 5)}</p>
                 </div>
-              </div>
-              <p className="font-semibold bg-black leading-[180px] px-20 text-white text-xl h-full">
-                19:00
-              </p>
-              <div className="flex justify-end w-full ">
-                <div className="flex items-center justify-center font-semibold text-center text-xl align-middle w-[60%]">
-                  Chelsea
-                </div>
-                <div className="flex justify-center w-[40%]">
-                  <div>
-                    <Image
-                      width={200}
-                      height={200}
-                      src={Team2}
-                      alt="hero_app"
-                      className="object-cover w-[120px] h-[120px] p-1"
-                    />
+                <div className="flex justify-end w-full ">
+                  <div className="md:flex hidden  items-center justify-center font-semibold text-center text-md xl:text-lg align-middle w-[60%] lg:w-[65%]">
+                    {item.away_team_name}
+                  </div>
+                  <div className="flex justify-center w-full lg:w-[35%]">
+                    <div className="w-[100px] h-[100px] md:w-[120px] md:h-[120px] p-5 md:p-1">
+                      <Image
+                        width={200}
+                        height={200}
+                        src={item.away_team_photo}
+                        alt="hero_app"
+                        className="object-contain w-full h-full"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          {/* card ends */}
-
-          {/* card */}
-          <div className="bg-white flex items-center gap-5 md:gap-5 justify-center lg:py-0 h-fit md:h-[180px]">
-            <div className="flex items-center gap-2 w-full px-16 h-full ">
-              <div className="flex w-full ">
-                <div className="flex justify-center w-[40%]">
-                  <div>
-                    <Image
-                      width={200}
-                      height={200}
-                      src={Team1}
-                      alt="hero_app"
-                      className="object-cover w-[120px] h-[120px] p-1"
-                    />
-                  </div>
-                </div>
-                <div className="flex items-center justify-center font-semibold text-center text-xl align-middle w-[60%]">
-                  Manchester United
-                </div>
-              </div>
-              <p className="font-semibold bg-black leading-[180px] px-20 text-white text-xl h-full">
-                19:00
-              </p>
-              <div className="flex justify-end w-full ">
-                <div className="flex items-center justify-center font-semibold text-center text-xl align-middle w-[60%]">
-                  Chelsea
-                </div>
-                <div className="flex justify-center w-[40%]">
-                  <div>
-                    <Image
-                      width={200}
-                      height={200}
-                      src={Team2}
-                      alt="hero_app"
-                      className="object-cover w-[120px] h-[120px] p-1"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
           {/* card ends */}
         </div>
       </div>
 
-      <div className="flex flex-col px-[190px] bg-white py-20">
-        <p className="text-3xl font-bold pb-10">ပြီးခဲ့သော ပွဲစဉ်များ</p>
+      <div className="flex flex-col px-3 md:px-5 lg:px-20 xl:px-[190px] bg-white py-10 lg:py-20">
+        <p className="md:text-xl lg:text-3xl font-bold pt-0 md:pt-5 pb-3 md:pb-5 lg:pb-10">
+          ပြီးခဲ့သော ပွဲစဉ်များ
+        </p>
 
-        <div className="flex flex-col gap-10">
-          {Data.map((item: any) => (
-            <div key={item} className="bg-[#fea61f] flex items-center gap-5 md:gap-5 justify-center lg:py-0 h-fit md:h-[180px]">
-              <div className="flex items-center gap-2 w-full px-16 h-full ">
+        <div className="flex flex-col gap-5 md:gap-5 lg:gap-10 ">
+          {matches?.map((item: any) => (
+            <div
+              key={item.id}
+              className="bg-[#fea61f] flex items-center gap-5 md:gap-5 justify-center lg:py-0 h-[100px] md:h-[140px] lg:h-[160px] xl:h-[180px]"
+            >
+              <div className="flex items-center gap-2 w-full lg:px-5 xl:px-16 h-full ">
                 <div className="flex w-full ">
-                  <div className="flex justify-center w-[40%]">
-                    <div>
+                  <div className="flex justify-center w-full lg:w-[35%]">
+                    <div className="w-[100px] h-[100px] md:w-[120px] md:h-[120px] p-5 md:p-1">
                       <Image
                         width={200}
                         height={200}
-                        src={Team1}
+                        src={item.home_team_photo}
                         alt="hero_app"
-                        className="object-cover w-[120px] h-[120px] p-1"
+                        className="object-contain w-full h-full"
                       />
                     </div>
                   </div>
-                  <div className="flex items-center justify-center font-semibold text-center text-xl align-middle w-[60%]">
-                    Manchester United
+                  <div className="md:flex hidden  items-center justify-center font-semibold text-center text-md xl:text-lg align-middle w-[60%] lg:w-[65%]">
+                    {item.home_team_name}
                   </div>
                 </div>
-                <p className="font-semibold bg-black leading-[180px] px-20 text-white text-xl h-full">
-                  19:00
-                </p>
+                <div className="flex flex-col gap-2 md:gap-3 md:pb-8 items-center justify-center w-[250px] xl:w-[400px] h-full font-semibold text-[#fea61f] bg-black ">
+                  <p className="w-full text-center text-sm md:text-base">
+                    {item.time.substr(0, 5)}
+                  </p>
+                  <p className="w-full text-center text-lg md:text-2xl">
+                    {item.home_team_score} - {item.away_team_score}
+                  </p>
+                </div>
                 <div className="flex justify-end w-full ">
-                  <div className="flex items-center justify-center font-semibold text-center text-xl align-middle w-[60%]">
-                    Chelsea
+                  <div className="md:flex hidden items-center justify-center font-semibold text-center text-md xl:text-lg align-middle w-[60%] lg:w-[65%]">
+                    {item.away_team_name}
                   </div>
-                  <div className="flex justify-center w-[40%]">
-                    <div>
+                  <div className="flex justify-center w-full lg:w-[35%]">
+                    <div className="w-[100px] h-[100px] md:w-[120px] md:h-[120px] p-5 md:p-1">
                       <Image
                         width={200}
                         height={200}
-                        src={Team2}
+                        src={item.away_team_photo}
                         alt="hero_app"
-                        className="object-cover w-[120px] h-[120px] p-1"
+                        className="object-contain w-full h-full"
                       />
                     </div>
                   </div>
